thirtythreeforty/dpfwebui

commit dadb40f315f3c5311eff69b5907276e6b73c8729

Rename lib to vendor and move patches
Also add a README.txt for the WAMR patch.

vendor/.gitignore
@@ -1,0 +1,3 @@
+wasm-micro-runtime
+wasmer
+Microsoft.Web.WebView2*

.gitignore
@@ -1,4 +1,3 @@
 .DS_Store
-lib
 bin
 build

Makefile.plugins.mk
@@ -4,14 +4,14 @@
 # ------------------------------------------------------------------------------
 # Basic setup
 
-HIPHOP_ROOT_PATH := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
-HIPHOP_SRC_PATH  ?= $(HIPHOP_ROOT_PATH)/src
-HIPHOP_LIB_PATH  ?= $(HIPHOP_ROOT_PATH)/lib
+HIPHOP_ROOT_PATH   := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
+HIPHOP_SRC_PATH    ?= $(HIPHOP_ROOT_PATH)/src
+HIPHOP_VENDOR_PATH ?= $(HIPHOP_ROOT_PATH)/vendor
 
-DPF_PATH         ?= $(HIPHOP_ROOT_PATH)/dpf
-DPF_TARGET_DIR   ?= bin
-DPF_BUILD_DIR    ?= build
-DPF_GIT_BRANCH   ?= develop
+DPF_PATH       ?= $(HIPHOP_ROOT_PATH)/dpf
+DPF_TARGET_DIR ?= bin
+DPF_BUILD_DIR  ?= build
+DPF_GIT_BRANCH ?= develop
 
 ifeq ($(HIPHOP_PROJECT_VERSION),)
 $(error HIPHOP_PROJECT_VERSION is not set)
@@ -257,7 +257,7 @@ endif
 
 ifeq ($(AS_DSP),true)
 ifeq ($(HIPHOP_WASM_RUNTIME),wamr)
-WAMR_PATH = $(HIPHOP_LIB_PATH)/wasm-micro-runtime
+WAMR_PATH = $(HIPHOP_VENDOR_PATH)/wasm-micro-runtime
 WAMR_BUILD_DIR = ${WAMR_PATH}/build
 WAMR_LIB_PATH = $(WAMR_BUILD_DIR)/libvmlib.a
 WAMR_GIT_URL = https://github.com/bytecodealliance/wasm-micro-runtime
@@ -290,12 +290,10 @@ $(WAMR_LIB_PATH): $(WAMR_PATH)
 WAMR_C_API_PATH = $(WAMR_PATH)/core/iwasm/common
 
 $(WAMR_PATH):
-	@mkdir -p $(HIPHOP_LIB_PATH)
-	@git -C $(HIPHOP_LIB_PATH) clone $(WAMR_GIT_URL)
-	@mv $(WAMR_C_API_PATH)/wasm_c_api.c $(WAMR_C_API_PATH)/wasm_c_api.c.bak
-	@cp $(HIPHOP_ROOT_PATH)/patch/wamr/wasm_c_api.c $(WAMR_C_API_PATH)
-	@mv $(WAMR_C_API_PATH)/wasm_c_api_internal.h $(WAMR_C_API_PATH)/wasm_c_api_internal.h.bak
-	@cp $(HIPHOP_ROOT_PATH)/patch/wamr/wasm_c_api_internal.h $(WAMR_C_API_PATH)
+	@mkdir -p $(HIPHOP_VENDOR_PATH)
+	@git -C $(HIPHOP_VENDOR_PATH) clone $(WAMR_GIT_URL)
+	@cp $(HIPHOP_VENDOR_PATH)/wamr-patch/wasm_c_api.c $(WAMR_C_API_PATH)
+	@cp $(HIPHOP_VENDOR_PATH)/wamr-patch/wasm_c_api_internal.h $(WAMR_C_API_PATH)
 endif
 endif
 
@@ -304,7 +302,7 @@ endif
 
 ifeq ($(AS_DSP),true)
 ifeq ($(HIPHOP_WASM_RUNTIME),wasmer)
-WASMER_PATH = $(HIPHOP_LIB_PATH)/wasmer
+WASMER_PATH = $(HIPHOP_VENDOR_PATH)/wasmer
 WASMER_VERSION = 2.1.1
 
 TARGETS += $(WASMER_PATH)
@@ -389,7 +387,7 @@ endif
 
 ifeq ($(WEB_UI),true)
 ifeq ($(WINDOWS),true)
-EDGE_WEBVIEW2_PATH = $(HIPHOP_LIB_PATH)/Microsoft.Web.WebView2
+EDGE_WEBVIEW2_PATH = $(HIPHOP_VENDOR_PATH)/Microsoft.Web.WebView2
 
 TARGETS += $(EDGE_WEBVIEW2_PATH)
 
@@ -404,9 +402,9 @@ $(error NuGet not found, try sudo apt install nuget or the equivalent for your d
 endif
 endif
 	@echo Downloading Edge WebView2 SDK
-	@mkdir -p $(HIPHOP_LIB_PATH)
+	@mkdir -p $(HIPHOP_VENDOR_PATH)
 	@eval $(MSYS_MINGW_SYMLINKS)
-	@nuget install Microsoft.Web.WebView2 -OutputDirectory $(HIPHOP_LIB_PATH)
+	@nuget install Microsoft.Web.WebView2 -OutputDirectory $(HIPHOP_VENDOR_PATH)
 	@ln -rs $(EDGE_WEBVIEW2_PATH).* $(EDGE_WEBVIEW2_PATH)
 
 /usr/bin/nuget.exe:

vendor/wamr-patch/wasm_c_api.c
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
  */
 
+// Patched to avoid engine singleton
+
 #include "wasm_c_api_internal.h"
 #include "wasm_memory.h"
 #include "wasm_runtime_common.h"

vendor/wamr-patch/wasm_c_api_internal.h
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
  */
 
+// Patched to avoid engine singleton
+
 #ifndef _WASM_C_API_INTERNAL_H
 #define _WASM_C_API_INTERNAL_H
 

commit 4a8677b9b97785d24792fc7c07b149e93b126da0

Fix CEF build on Linux
Broken by dadb40f

src/linux/Makefile.cef.mk
@@ -7,7 +7,7 @@
 
 CEF_DISTRO = cef_binary_93.1.12+ga8ffe4b+chromium-93.0.4577.82_linux64_minimal
 CEF_DISTRO_FILE = $(CEF_DISTRO).tar.bz2
-CEF_PATH = $(HIPHOP_LIB_PATH)/cef
+CEF_PATH = $(HIPHOP_VENDOR_PATH)/cef
 CEF_BIN_PATH = $(CEF_PATH)/Release
 CEF_RES_PATH = $(CEF_PATH)/Resources
 CEF_URL = https://cef-builds.spotifycdn.com/$(CEF_DISTRO_FILE)
@@ -18,15 +18,15 @@ CEF_URL = https://cef-builds.spotifycdn.com/$(CEF_DISTRO_FILE)
 $(CEF_PATH):
 	@echo Downloading CEF
 	@wget -4 -O /tmp/$(CEF_DISTRO_FILE) $(CEF_URL)
-	@mkdir -p $(HIPHOP_LIB_PATH)
+	@mkdir -p $(HIPHOP_VENDOR_PATH)
 	@echo Decompressing CEF, this takes a while!
-	@tar xjf /tmp/$(CEF_DISTRO_FILE) -C $(HIPHOP_LIB_PATH)
+	@tar xjf /tmp/$(CEF_DISTRO_FILE) -C $(HIPHOP_VENDOR_PATH)
 	@ln -s $(CEF_DISTRO) $(CEF_PATH)
 	@rm /tmp/$(CEF_DISTRO_FILE)
 
 # ------------------------------------------------------------------------------
 # CEF C++ wrapper static library. Based on libcef_dll/CMakeLists.txt
-# Library saved to framework HIPHOP_LIB_PATH not plugin BUILD_DIR
+# Library saved to framework HIPHOP_VENDOR_PATH not plugin BUILD_DIR
 
 CEF_FILES_WRAPPER = \
 	cpptoc/frame_handler_cpptoc.cc \
@@ -217,7 +217,7 @@ CEF_CXXFLAGS = -I$(CEF_PATH) -DNDEBUG -DWRAPPING_CEF_SHARED -D_FILE_OFFSET_BITS=
 
 CEF_WRAPPER_OBJ = $(CEF_FILES_WRAPPER:%.cc=$(BUILD_DIR)/libcef_dll_wrapper/%.o)
 
-CEF_WRAPPER_LIB = $(HIPHOP_LIB_PATH)/libcef_dll_wrapper.a
+CEF_WRAPPER_LIB = $(HIPHOP_VENDOR_PATH)/libcef_dll_wrapper.a
 
 $(CEF_WRAPPER_LIB): $(CEF_WRAPPER_OBJ)
 	@ar qc $(CEF_WRAPPER_LIB) $(CEF_WRAPPER_OBJ)
@@ -245,7 +245,7 @@ LXHELPER_OBJ = $(LXHELPER_SRC:%=$(LXHELPER_BUILD_DIR)/%.o)
 LXHELPER_CFLAGS = -I$(HIPHOP_SRC_PATH) -I$(DPF_PATH) -I$(CEF_PATH) \
                   -DCEF_HELPER_BINARY -DPLUGIN_BIN_BASENAME=$(NAME)
 
-LXHELPER_LDFLAGS = -ldl -lXi -lcef_dll_wrapper -L$(HIPHOP_LIB_PATH) -lcef -L$(CEF_BIN_PATH) \
+LXHELPER_LDFLAGS = -ldl -lXi -lcef_dll_wrapper -L$(HIPHOP_VENDOR_PATH) -lcef -L$(CEF_BIN_PATH) \
 				   -lX11 -O3 -DNDEBUG -rdynamic -fPIC -pthread -Wl,--disable-new-dtags \
 				   -Wl,--fatal-warnings -Wl,-rpath,. -Wl,-z,noexecstack \
 				   -Wl,-z,now -Wl,-z,relro -m64 -Wl,-O1 -Wl,--as-needed \

vendor/.gitignore
@@ -1,3 +1,4 @@
+cef*
 wasm-micro-runtime
 wasmer
 Microsoft.Web.WebView2*